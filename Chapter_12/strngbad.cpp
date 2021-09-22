de27f3354bddf67dbac103ca1ab96272a897ab03
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-09-22 18:39:43 +0800

    12.1.1 strngbad示例和静态类成员

--- Chapter_12/strngbad.cpp
+++ Chapter_12/strngbad.cpp
@@ -4,6 +4,9 @@
 using std::cout;
 
 // initializing static class member
+// 初始化静态类成员变量--不能在类声明中初始化静态类成员变量
+// 可以在类声明之外单独进行静态成员变量初始化--因为静态成员变量单独存储，而不是对象的组成部分\
+// 如果在头文件中进行初始化，将出现等多个初始化语句副本，从而引发错误？？--不理解
 int StringBad::num_strings = 0;
 
 // class methods
@@ -11,8 +14,11 @@ int StringBad::num_strings = 0;
 // construct StringBad from C string
 StringBad::StringBad(const char * s)
 {
+    // 计算字符串的长度
     len = std::strlen(s);             // set size
+    // 使用new分配足够的空间来保存字符串，并将新内存的地址赋给str
     str = new char[len + 1];          // allot storage
+    // 将形参s传递的字符串负载到新分配的内存中
     std::strcpy(str, s);              // initialize pointer
     // 所有类对象共享同一个静态成员变量
     num_strings++;                    // set object count
@@ -33,7 +39,7 @@ StringBad::StringBad()                // default constructor
 StringBad::~StringBad()               // necessary destructor
 {
     cout << "\"" << str << "\" object deleted, ";    // FYI
-    --num_strings;                    // required
+    --num_strings;// 析构一个对象,num_strings-1                    // required
     cout << num_strings << " left\n"; // FYI
     delete [] str;                    // required
 }
--- Chapter_12/strngbad.h
+++ Chapter_12/strngbad.h
@@ -9,6 +9,7 @@ private:
     int len;                   // length of string
 
     // 静态成员变量--所有类对象共享同一个静态类成员
+    // 如果静态成员变量为const整数类型或枚举型，则可以在类生命中进行初始化
     static int num_strings;    // number of objects
 public:
     StringBad(const char * s); // constructor
