e9fd7913df1ad152570f80ef256793c64b34e8b0
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-10-02 21:04:05 +0800

    13.1 基类

---
+++ Chapter_13/tabtenn0.h
@@ -0,0 +1,21 @@
+// tabtenn0.h -- a table-tennis base class
+#ifndef TABTENN0_H_
+#define TABTENN0_H_
+#include <string>
+using std::string;
+// simple base class
+class TableTennisPlayer
+{
+private:
+    string firstname;
+    string lastname;
+    bool hasTable;
+public:
+    // 自定义构造函数--使用到复制构造函数
+    TableTennisPlayer (const string & fn = "none",
+                       const string & ln = "none", bool ht = false);
+    void Name() const;
+    bool HasTable() const { return hasTable; };
+    void ResetTable(bool v) { hasTable = v; };
+};
+#endif
