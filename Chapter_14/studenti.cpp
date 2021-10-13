97940af662cb3cc0e262c155d2e0c82cc9356992
gaoziqianghi@163.com <gaoziqianghi@163.com>
2021-10-13 11:50:06 +0800

    14.2 私有继承

---
+++ Chapter_14/studenti.cpp
@@ -0,0 +1,77 @@
+// studenti.cpp -- Student class using private inheritance
+#include "studenti.h"
+using std::ostream;
+using std::endl;
+using std::istream;
+using std::string;
+
+// public methods
+// 访问基类的方法
+double Student::Average() const
+{
+    if (ArrayDb::size() > 0)
+        return ArrayDb::sum()/ArrayDb::size();  
+    else
+        return 0;
+}
+
+// 访问基类的对象
+const string & Student::Name() const
+{
+    return (const string &) *this;
+}
+
+double & Student::operator[](int i)
+{
+    return ArrayDb::operator[](i);         // use ArrayDb::operator[]()
+}
+
+double Student::operator[](int i) const
+{
+    return ArrayDb::operator[](i);
+}
+
+// private method
+ostream & Student::arr_out(ostream & os) const
+{
+    int i;
+    int lim = ArrayDb::size();
+    if (lim > 0)
+    {
+        for (i = 0; i < lim; i++)
+        {
+            os << ArrayDb::operator[](i) << " ";
+            if (i % 5 == 4)
+                os << endl;
+        }
+        if (i % 5 != 0)
+            os << endl;
+    }
+    else
+        os << " empty array ";
+    return os; 
+}
+
+// friends
+// use String version of operator>>()
+istream & operator>>(istream & is, Student & stu)
+{
+    is >> (string &)stu;
+    return is; 
+}
+
+// use string friend getline(ostream &, const string &)
+istream & getline(istream & is, Student & stu)
+{
+    getline(is, (string &)stu);
+    return is;
+}
+
+// 使用基类的友元函数
+// use string version of operator<<()
+ostream & operator<<(ostream & os, const Student & stu)
+{
+    os << "Scores for " << (const string &) stu  << ":\n";
+    stu.arr_out(os);  // use private method for scores
+    return os;
+}
